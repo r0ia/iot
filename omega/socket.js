edf4bf11c54b62ef052ae1e3b443e9445615ba31
MikeD <mikeddolha@gmail.com>
2017-08-27 02:30:09 +0300

    Finalize oled commands

--- omega/socket.js
+++ omega/socket.js
@@ -1,11 +1,17 @@
 module.exports = function(server_url) {
   var socket = require('socket.io-client')(server_url);
+  var oled = require('onion-omega-oled-text');
+  oled.init().then(function() {
+    oled.writeText("Hello");
+    oled.setStandbyTimeout(10);
+  });
 
   socket.on('connect', function() {
-    console.log('connected to backend! yaaay');
+    oled.writeText('Connected');
   });
 
   socket.on('unlocked', function(data) {
-    console.log('unlocked -> ', JSON.stringify(data));
-  })
+    var newLine = data.firstName + '\n' + data.lastName + '\nunlocked';
+    oled.writeText(newLine);
+  });
 };
